304557e2ceeeb80cec693c62596fa9d8ed3caf21
John Shepler <john.sheplerjr@gmail.com>
2017-06-05 09:32:10 -0600

    nav links

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,19 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Ingredients } from '../shared/ingredient.model'
+import { Ingredient } from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-    ingredients: Ingredients[] = [
-        new Ingredients('Apples', 5),
-        new Ingredients('Tomatoes', 10),
+    ingredient: Ingredient[] = [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 10),
     ];
   constructor() { }
 
   ngOnInit() {
   }
 
+  onIngredientAdded(ingredient: Ingredient) {
+      this.ingredient.push(ingredient)
+  }
 }
